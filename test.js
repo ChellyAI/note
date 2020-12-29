4957f01de3165707da37bbe674203b9dad028df7
caisiqi <cai.siqi@datatom.com>
2020-12-29 18:28:03 +0800

    feat: 补充了路由原理，和Symbol类型的描述

--- test.js
+++ test.js
@@ -1,6 +1,40 @@
-var x = 1;
-if(var f = 2){
-    x += typeof f;
+const bar = function () {
+    console.log(this.name + this.age + this.school);
+
+    return 'qwer';
 }
 
-console.log(x);
+const p1 = {
+    name: 'caisiqi',
+    age: 16,
+    school: 'cug',
+};
+
+bar.call(p1);
+
+Function.prototype.call2 = function (obj = window, ...args) {
+    const func = Symbol();
+    obj[func] = this;
+
+    const result = obj[func](...args);
+    delete obj[func];
+    return result;
+}
+
+Function.prototype.apply2 = function(obj = window, props) {
+    const func = Symbol();
+    obj[func] = this;
+
+    const result = obj[func](...props);
+    delete obj[func];
+    return result;
+}
+
+Function.prototype.bind2 = function(obj = window, ...args) {
+    const func = Symbol();
+    obj[func] = this;
+
+    return obj[func];
+}
+
+bar.call2(p1);
