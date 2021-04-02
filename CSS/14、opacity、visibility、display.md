c3ec4089ca921571f4ed20dd9865c5da58a281a3
caisiqi <cai.siqi@datatom.com>
2021-04-02 18:19:19 +0800

    feat: 开始补充css隐藏元素的内容

--- CSS/14、opacity、visibility、display.md
+++ CSS/14、opacity、visibility、display.md
@@ -4,7 +4,7 @@ opacity 将元素变透明，如果其中某个透明的按钮有绑定事件，
 
 visibility 相比 opacity 能够让事件不被触发
 
-display:none 则直接将整个 DOM 结构去掉了
+display:none：元素不可见，不占据空间，无法响应点击事件
 
 使用记录：
 
--- qwer.js
+++ qwer.js
@@ -1,62 +1,94 @@
-const func = (num) => {
-  const all = Array.from({length: 10000}, (_, i) => i+2);
+// const func = (num) => {
+//   const all = Array.from({length: 10000}, (_, i) => i+2);
 
-  const result = [];
+//   const result = [];
 
-  for (let i = 0; i < num; i++) {
-    result.push(
-      all.splice(
-        Math.floor(Math.random() * all.length),
-        1,
-      )[0]
-    )
+//   for (let i = 0; i < num; i++) {
+//     result.push(
+//       all.splice(
+//         Math.floor(Math.random() * all.length),
+//         1,
+//       )[0]
+//     )
+//   }
+
+//   return result;
+// }
+
+// // console.log(func(10000))
+
+// function fib(n) {
+//   let fiber = {
+//     arg: n,
+//     returnAddr: null,
+//     a: 0,
+//   };
+
+//   // 标记循环
+//   rec: while (true) {
+//     // 当展开完全后，开始计算
+//     if (fiber.arg <= 2) {
+//       let sum = 1;
+
+//       // 寻找父级
+//       while (fiber.returnAddr) {
+//         fiber = fiber.returnAddr;
+
+//         if (fiber.a === 0) {
+//           fiber.a = sum;
+//           fiber = {
+//             arg: fiber.arg - 2,
+//             returnAddr: fiber,
+//             a: 0,
+//           };
+
+//           continue rec;
+//         }
+
+//         sum += fiber.a;
+//       }
+//       return sum;
+//     } else {
+//       // 先展开
+//       fiber = {
+//         arg: fiber.arg - 1,
+//         returnAddr: fiber,
+//         a: 0,
+//       };
+//     }
+//   }
+// }
+
+// console.log(fib(6));
+class Person {
+  constructor(name) {
+    this.name = name;
   }
 
-  return result;
-}
-
-// console.log(func(10000))
-
-function fib(n) {
-  let fiber = {
-    arg: n,
-    returnAddr: null,
-    a: 0,
-  };
-
-  // 标记循环
-  rec: while (true) {
-    // 当展开完全后，开始计算
-    if (fiber.arg <= 2) {
-      let sum = 1;
-
-      // 寻找父级
-      while (fiber.returnAddr) {
-        fiber = fiber.returnAddr;
-
-        if (fiber.a === 0) {
-          fiber.a = sum;
-          fiber = {
-            arg: fiber.arg - 2,
-            returnAddr: fiber,
-            a: 0,
-          };
-
-          continue rec;
-        }
-
-        sum += fiber.a;
-      }
-      return sum;
-    } else {
-      // 先展开
-      fiber = {
-        arg: fiber.arg - 1,
-        returnAddr: fiber,
-        a: 0,
-      };
-    }
+  run = () => {
+    console.log('haha', this.name);
   }
 }
 
-console.log(fib(6));
+class Chinese extends Person {
+  constructor(name, age) {
+    super();
+    this.name = name;
+    this.age = age;
+  }
+
+  fly = function() {
+    console.log('起飞', this.name, this.age);
+  }
+}
+
+const person = new Person('人');
+
+const caisiqi = new Chinese('caisiqi', 16);
+
+person.run();
+
+caisiqi.run();
+
+caisiqi.fly();
+
